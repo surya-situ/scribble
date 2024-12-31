516b06f13a376140034ea0a43c59b7969692507b
surya-situ <suryasocial55@gmail.com>
2024-12-31 17:41:57 +0530

    feat: Add Note component with edit dialog functionality

---
+++ src/components/Note.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import { Note as NoteModel } from "@prisma/client";
+import React, { useState } from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import AddEditNoteDialog from "./AddEditNoteDialog";
+
+interface NoteProps {
+  note: NoteModel;
+}
+
+export default function Note({ note }: NoteProps) {
+  const [showEditDialog, setShowEditDialog] = useState(false);
+
+  const wasUpdated = note.updatedAt > note.createdAt;
+  const createdUpdatedTimestamp = (
+    wasUpdated ? note.updatedAt : note.createdAt
+  ).toDateString();
+
+  return (
+    <>
+      <Card className="cursor-pointer hover:shadow-lg transition-shadow" onClick={() => setShowEditDialog(true)}>
+        <CardHeader>
+          <CardTitle>{note.title}</CardTitle>
+          <CardDescription>
+            {createdUpdatedTimestamp}
+            {wasUpdated && " (updated)"}
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p className="whitespace-pre-line">{note.content}</p>
+        </CardContent>
+      </Card>
+      <AddEditNoteDialog open={showEditDialog} setOpen={setShowEditDialog} noteToEdit={note}/>
+    </>
+  );
+}
