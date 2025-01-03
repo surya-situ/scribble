e7987ffc1d97328c4292291db11671cc3d0e3733
surya-situ <suryasocial55@gmail.com>
2025-01-03 16:41:41 +0530

    feat: add Layout component with Navbar and main content area

---
+++ src/app/notes/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Navbar from "./Navbar";
+
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Navbar />
+      <main className="m-auto max-w-7xl p-4">{children}</main>
+    </>
+  );
+}
